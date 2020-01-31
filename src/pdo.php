9d5b8ada2d87fc9d5e19b3e9a1363db06b02bea2
Protocol Corporation <39626142+ProtocolLive@users.noreply.github.com>
2020-01-30 21:32:51 -0300

    Fixing the error display

--- src/pdo.php
+++ src/pdo.php
@@ -1,7 +1,7 @@
 <?php
 // Protocol Corporation Ltda.
 // https://github.com/ProtocolLive/PhpLive/
-// Version 2020-01-30-00
+// Version 2020-01-30-01
 
 $DbLastConn = null;
 $DbPrefix = null;
@@ -18,14 +18,19 @@ $ErrPdo = [
   "HY093" => "Quantidade incorreta de parâmetros especificado"
 ];
 
-function Erro($msg){
+function Erro($Number, $Msg){
   // Backtrace = 1
   $Debug = 1;
   
   $debug = debug_backtrace();
   $debug = end($debug);
   echo "<br>";
-  echo $msg . " <b>em</b> " . $debug["file"] . " <b>na linha</b> " . $debug["line"];
+  if(ini_get("display_errors") == true or isset($ErrPdo[$Number]) == false){
+    echo $Msg;
+  }else{
+    echo $ErrPdo[$Number];
+  }
+  echo " <b>em</b> " . $debug["file"] . " <b>na linha</b> " . $debug["line"];
   if(($Debug & 1) == 1){
     echo "<pre>";
     var_dump(debug_backtrace());
@@ -46,30 +51,23 @@ function Erro($msg){
  */
 function SqlConnect($Options = []){
   global $ErrPdo, $DbLastConn, $DbPrefix;
+  set_error_handler("Erro");
   if(isset($Options["Drive"]) == false) $Options["Drive"] = "mysql";
   if(isset($Options["Charset"]) == false) $Options["Charset"] = "utf8";
   if(isset($Options["TimeOut"]) == false) $Options["TimeOut"] = 5;
-  try{
-    if(isset($Options["Prefix"])){
-      $DbPrefix = $Options["Prefix"];
-    }
-    $DbLastConn = new PDO(
-      $Options["Drive"] . ":host=" . $Options["Ip"] . ";dbname=" . $Options["Db"] . ";charset=" . $Options["Charset"],
-      $Options["User"],
-      $Options["Pwd"]
-    );
-    $DbLastConn->setAttribute(PDO::ATTR_TIMEOUT, $Options["TimeOut"]);
-    if(ini_get("display_errors") == true){
-      $DbLastConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION || PDO::ERRMODE_WARNING);
-    }
-    return $DbLastConn;
-  }catch(PDOException $e){
-    if(ini_get("display_errors") == true or isset($ErrPdo[$e->getCode()]) == false){
-      Erro($e->getMessage());
-    }else{
-      Erro($ErrPdo[$e->getCode()]);
-    }
+  if(isset($Options["Prefix"])){
+    $DbPrefix = $Options["Prefix"];
   }
+  $DbLastConn = new PDO(
+    $Options["Drive"] . ":host=" . $Options["Ip"] . ";dbname=" . $Options["Db"] . ";charset=" . $Options["Charset"],
+    $Options["User"],
+    $Options["Pwd"]
+  );
+  $DbLastConn->setAttribute(PDO::ATTR_TIMEOUT, $Options["TimeOut"]);
+  if(ini_get("display_errors") == true){
+    $DbLastConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION || PDO::ERRMODE_WARNING);
+  }
+  return $DbLastConn;
 }
 
 /**
@@ -84,61 +82,54 @@ function SqlConnect($Options = []){
  */
 function SQL($Query, $Params = null, $Options = []){
   global $ErrPdo, $DbLastConn, $DbPrefix;
-  try{
-    if(isset($Options["Conn"]) == false){
-      if($DbLastConn == null){
-        Erro("Você não iniciou uma conexão a um banco de dados");
-      }else{
-        $Options["Conn"] = &$DbLastConn;
-      }
-    }
-    $Query = Clean($Query);
-    if($DbPrefix != null){
-      $Query = str_replace("##", $DbPrefix . "_", $Query);
-    }
-    $comando = explode(" ", $Query);
-    $comando = strtolower($comando[0]);
-    $result = $Options["Conn"]->prepare($Query);
-    if($Params != null){
-      foreach($Params as &$Param){
-        if(count($Param) != 3){
-          Erro("Quantidade incorreta de parâmetros ao especificar um placehole");
-        }else{
-          if($Param[2] == PDO::PARAM_INT){
-            $Param[1] = str_replace(",", ".", $Param[1]);
-            if(strpos($Param[1], ".") !== false){
-              $Param[2] = PDO::PARAM_STR;
-            }
-          }elseif($Param[2] == PDO::PARAM_BOOL){
-            $Param[1] = $Param[1] == "true"? true: false;
-          }
-          $result->bindValue($Param[0], $Param[1], $Param[2]);
-        }
-      }
-    }
-    $result->execute();
-    if(isset($Options["Debug"]) and $Options["Debug"] == true){?>
-      <pre style="text-align:left"><?php $result->debugDumpParams();?></pre><?php
-    }
-    if($comando == "select" or $comando == "show" or $comando == "call"){
-      $retorno = $result->fetchAll();
-    }elseif($comando == "insert"){
-      $retorno = $Options["Conn"]->lastInsertId();
-    }
-    if(isset($Options["Log"]) and $Options["Log"] != null and 
-    isset($Options["User"]) and $Options["User"] != null){
-      if(isset($Options["Target"]) == false) $Options["Target"] = null;
-      SqlLog($Options["User"], $result->debugDumpParams(), $Options["Log"], $Options["Target"], $Options["Conn"]);
-    }
-    if(isset($retorno)){
-      return $retorno;
-    }
-  }catch(PDOException $e){
-    if(ini_get("display_errors") == true or isset($ErrPdo[$e->getCode()]) == false){
-      Erro($e->getMessage());
+  set_error_handler("Erro");
+  if(isset($Options["Conn"]) == false){
+    if($DbLastConn == null){
+      Erro("Você não iniciou uma conexão a um banco de dados");
     }else{
-      Erro($ErrPdo[$e->getCode()]);
+      $Options["Conn"] = &$DbLastConn;
     }
+  }
+  $Query = Clean($Query);
+  if($DbPrefix != null){
+    $Query = str_replace("##", $DbPrefix . "_", $Query);
+  }
+  $comando = explode(" ", $Query);
+  $comando = strtolower($comando[0]);
+  $result = $Options["Conn"]->prepare($Query);
+  if($Params != null){
+    foreach($Params as &$Param){
+      if(count($Param) != 3){
+        Erro("Quantidade incorreta de parâmetros ao especificar um placehole");
+      }else{
+        if($Param[2] == PDO::PARAM_INT){
+          $Param[1] = str_replace(",", ".", $Param[1]);
+          if(strpos($Param[1], ".") !== false){
+            $Param[2] = PDO::PARAM_STR;
+          }
+        }elseif($Param[2] == PDO::PARAM_BOOL){
+          $Param[1] = $Param[1] == "true"? true: false;
+        }
+        $result->bindValue($Param[0], $Param[1], $Param[2]);
+      }
+    }
+  }
+  $result->execute();
+  if(isset($Options["Debug"]) and $Options["Debug"] == true){?>
+    <pre style="text-align:left"><?php $result->debugDumpParams();?></pre><?php
+  }
+  if($comando == "select" or $comando == "show" or $comando == "call"){
+    $retorno = $result->fetchAll();
+  }elseif($comando == "insert"){
+    $retorno = $Options["Conn"]->lastInsertId();
+  }
+  if(isset($Options["Log"]) and $Options["Log"] != null and 
+  isset($Options["User"]) and $Options["User"] != null){
+    if(isset($Options["Target"]) == false) $Options["Target"] = null;
+    SqlLog($Options["User"], $result->debugDumpParams(), $Options["Log"], $Options["Target"], $Options["Conn"]);
+  }
+  if(isset($retorno)){
+    return $retorno;
   }
 }
 
