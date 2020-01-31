efc0c458ca13015241b34ca89fc10d83fef6be33
Protocol Corporation <39626142+ProtocolLive@users.noreply.github.com>
2020-01-30 22:27:13 -0300

    Default groups and create order

--- docs/perms.sql
+++ docs/perms.sql
@@ -2,6 +2,12 @@ CREATE TABLE `sys_groups` (
   `group_id` int unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `group` varchar(45) NOT NULL UNIQUE KEY
 );
+insert into sys_groups(`group`) values('Everyone'),('Authenticated users'),('Administrators');
+
+CREATE TABLE `sys_resources` (
+  `resource_id` int unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `resource` varchar(45) NOT NULL UNIQUE KEY
+);
 
 CREATE TABLE `sys_perms` (
   `perm_id` int unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -13,11 +19,6 @@ CREATE TABLE `sys_perms` (
   `o` tinyint unsigned NOT NULL DEFAULT 0,
   FOREIGN KEY (`resource_id`) REFERENCES `sys_resources` (`resource_id`),
   FOREIGN KEY (`group_id`) REFERENCES `sys_groups` (`group_id`)
-);
-
-CREATE TABLE `sys_resources` (
-  `resource_id` int unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `resource` varchar(45) NOT NULL UNIQUE KEY
 );
 
 CREATE TABLE `sys_usergroup` (
