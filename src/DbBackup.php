006ac99e576822c9e7714acd17cf03e74f9e0a36
Protocol Corporation <39626142+ProtocolLive@users.noreply.github.com>
2020-03-23 10:33:04 -0300

    Using the PHP function addslashes

--- src/DbBackup.php
+++ src/DbBackup.php
@@ -1,7 +1,7 @@
 <?php
 // Protocol Corporation Ltda.
 // https://github.com/ProtocolLive/PhpLive/
-// Version 2020-03-20-04
+// Version 2020-03-23-00
 
 function DbBackup($Options = [], $PhpLivePdo = "PDO"){
   global $$PhpLivePdo;
@@ -123,7 +123,7 @@ function DbBackup($Options = [], $PhpLivePdo = "PDO"){
             if($result[$i][$j] == ""){
               fwrite($file, "null");
             }elseif(is_numeric($result[$i][$j]) == false){
-              fwrite($file, "'" . str_replace("'", "''", $result[$i][$j]) . "'");
+              fwrite($file, "'" . addslashes($result[$i][$j]));
             }else{
               fwrite($file, $result[$i][$j]);
             }
