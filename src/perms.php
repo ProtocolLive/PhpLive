60f6210c5df2eef9364c1092839a20160c1e6140
Protocol Corporation <39626142+ProtocolLive@users.noreply.github.com>
2020-03-27 10:38:37 -0300

    Forget Resource outside the Options

--- src/perms.php
+++ src/perms.php
@@ -1,7 +1,7 @@
 <?php
 // Protocol Corporation Ltda.
 // https://github.com/ProtocolLive/PhpLive/
-// Version 2020-03-27-00
+// Version 2020-03-27-01
 
 class PhpLivePerms{
   private $PhpLivePdo = null;
@@ -23,31 +23,31 @@ class PhpLivePerms{
 
     $return = ["r" => null, "w" => null, "o" => null];
     //Get resource id by name
-    if(is_numeric($Resource) == false){
+    if(is_numeric($Options["Resource"]) == false){
       if(session_name() == "PHPSESSID"){
         $result = $PhpLivePdo->SQL("select resource_id
           from sys_resources
           where resource=?
             and site is null", [
-          [1, $Resource, PdoStr]
+          [1, $Options["Resource"], PdoStr]
         ]);
       }else{
         $result = $PhpLivePdo->SQL("select resource_id
           from sys_resources
           where resource=?
             and site=?", [
-          [1, $Resource, PdoStr],
+          [1, $Options["Resource"], PdoStr],
           [2, session_name(), PdoStr]
         ]);
       }
-      $Resource = $result[0][0];
+      $Options["Resource"] = $result[0][0];
     }
     // Permissions for everyone
     $result = $PhpLivePdo->SQL("select r,w,o
       from sys_perms
       where resource_id=?
         and group_id=1", [
-      [1, $Resource, PdoInt]
+      [1, $Options["Resource"], PdoInt]
     ]);
     if(count($result) > 0){
       $return = $SetPerms($return, $result[0]);
@@ -76,7 +76,7 @@ class PhpLivePerms{
           or group_id in (select group_id from sys_groups where user_id=:user)
         )
       order by r,w,o", [
-      [":resource", $Resource, PdoInt],
+      [":resource", $Options["Resource"], PdoInt],
       [":user", $Options["User"], PdoInt]
     ]);
     if(count($result) > 0){
