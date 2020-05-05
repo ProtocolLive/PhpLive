7908163f22d5750a84a8f9049d240c6bda7c9c58
Protocol Corporation <39626142+ProtocolLive@users.noreply.github.com>
2020-05-05 17:55:10 -0300

    Single quote and new block id

--- src/cpf.php
+++ src/cpf.php
@@ -1,48 +1,48 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/protocollive
-//Version 2020.04.27.00
+//Version 2020.05.05.00
 
 function Cpf(string $Cpf):boolean{
-  $Cpf = str_replace(".", "", $Cpf);
-  $Cpf = str_replace("-", "", $Cpf);
-  $Erro = ["00000000000", "11111111111", "22222222222", "33333333333", "44444444444",
-  "55555555555", "66666666666", "77777777777", "88888888888", "99999999999"
+  $Cpf = str_replace('.', '', $Cpf);
+  $Cpf = str_replace('-', '', $Cpf);
+  $Erro = ['00000000000', '11111111111', '22222222222', '33333333333', '44444444444',
+  '55555555555', '66666666666', '77777777777', '88888888888', '99999999999'
   ];
-  if(strlen($Cpf) != 11 or in_array($Cpf, $Erro)){
+  if(strlen($Cpf) != 11 or in_array($Cpf, $Erro)):
     return false;
-  }
+  endif;
 
   $temp = substr($Cpf, 0, -2);
   $temp = str_split($temp);
   $temp = array_reverse($temp);
 
   $c1 = 0;
-  for($i = 0; $i < 9; $i++){
+  for($i = 0; $i < 9; $i++):
     $c1 += $temp[$i] * ($i + 2);
-  }
+  endfor;
   $c1 %= 11;
-  if($c1 < 2){
+  if($c1 < 2):
     $c1 = 0;
-  }else{
+  else:
     $c1 = 11 - $c1;
-  }
+  endif;
   $temp = array_merge([$c1], $temp);
 
   $c2 = 0;
-  for($i = 0; $i < 10; $i++){
+  for($i = 0; $i < 10; $i++):
     $c2 += $temp[$i] * ($i + 2);
-  }
+  endfor;
   $c2 %= 11;
-  if($c2 < 2){
+  if($c2 < 2):
     $c2 = 0;
-  }else{
+  else:
     $c2 = 11 - $c2;
-  }
+  endif;
 
-  if(substr($Cpf, -2) == $c1 . $c2){
+  if(substr($Cpf, -2) == $c1 . $c2):
     return true;
-  }else{
+  else:
     return false;
-  }
+  endif;
 }
