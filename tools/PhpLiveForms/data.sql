e69eca6b49ce4c64cc16980c68646cbc4e2fc1be
Protocol Corporation <39626142+ProtocolLive@users.noreply.github.com>
2020-05-05 13:22:35 -0300

    Default data to run the tool

---
+++ tools/PhpLiveForms/data.sql
@@ -0,0 +1,23 @@
+insert into forms_forms values
+(1,'admin','form','ajax',null,0),
+(2,'admin','fields','ajax',null,0);
+
+insert into forms_fields values
+(1,1,'Site','site','text',null,0,null,null,null,null,null,100),
+(2,1,'Form','form','text',null,0,null,null,null,null,null,100),
+(3,1,'Method','method','select','ajax',0,null,null,null,null,null,100),
+(4,1,'Action','action','text',null,0,null,null,null,null,null,100),
+(5,1,'Auto complete','autocomplete','select',0,0,null,null,null,null,null,100),
+(6,1,'Send',null,'submit',null,0,null,null,null,null,null,100),
+(7,2,'Label','label','text',null,0,null,null,null,null,null,100),
+(8,2,'Name','name','text',null,0,null,null,null,null,null,100),
+(9,2,'Type','type','select',null,0,null,null,null,null,null,100),
+(10,2,'Mode','mode','select',0,0,null,null,null,null,null,100),
+(11,2,'Order','order','number',100,0,null,null,null,null,null,100),
+(12,2,'Default','default','text',null,0,null,null,null,null,null,100),
+(13,2,'Size','size','number',null,0,null,null,null,null,null,100),
+(14,2,'Estyle','style','text',null,0,null,null,null,null,null,100),
+(15,2,'Class','class','text',null,0,null,null,null,null,null,100),
+(16,2,'JS event','js_event','text',null,0,null,null,null,null,null,100),
+(17,2,'JS code','js_code','text',null,0,null,null,null,null,null,100),
+(18,2,'Send',null,'submit',null,0,null,null,null,null,null,100);
