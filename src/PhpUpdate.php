0a7e3577b37c26cc47b29885f3cb672a21bb6a78
Protocol Corporation <39626142+ProtocolLive@users.noreply.github.com>
2020-05-05 21:06:02 -0300

    Single quotes and new block id

--- src/PhpUpdate.php
+++ src/PhpUpdate.php
@@ -1,18 +1,18 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/PhpLive
-//Version 2020.04.27.00
+//Version 2020.05.05.00
 
 function PhpUpdate():string{
-  if(extension_loaded("openssl") == false){
+  if(extension_loaded('openssl') == false):
     return false;
-  }
-  $ini = ini_get("default_socket_timeout");
-  ini_set("default_socket_timeout", 1);
-  $text = @file_get_contents("https://www.php.net/downloads");
-  ini_set("default_socket_timeout", $ini);
-  $text = substr($text, strpos($text, "Current Stable"));
-  $text = substr($text, strpos($text, "PHP") + 4);
-  $text = substr($text, 0, strpos($text, " "));
+  endif;
+  $ini = ini_get('default_socket_timeout');
+  ini_set('default_socket_timeout', 1);
+  $text = @file_get_contents('https://www.php.net/downloads');
+  ini_set('default_socket_timeout', $ini);
+  $text = substr($text, strpos($text, 'Current Stable'));
+  $text = substr($text, strpos($text, 'PHP') + 4);
+  $text = substr($text, 0, strpos($text, ' '));
   return $text;
 }
