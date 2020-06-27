20b066ada33e752bb73d0273ba112e069fc5e5f8
Protocol Corporation <39626142+ProtocolLive@users.noreply.github.com>
2020-06-27 13:06:36 -0300

    Fixed types

--- src/Perms/perms74.php
+++ src/Perms/perms74.php
@@ -1,7 +1,7 @@
 <?php
 // Protocol Corporation Ltda.
 // https://github.com/ProtocolLive/PhpLive/
-// Version 2020.05.26.00
+// Version 2020.06.27.00
 
 class PhpLivePerms{
   private ?PhpLivePdo $PhpLivePdo = null;
@@ -12,7 +12,7 @@ class PhpLivePerms{
 
   public function Access(array $Options):array{
     if($this->PhpLivePdo === null):
-      if(isset($Options['PhpLivePdo']) == false):
+      if(isset($Options['PhpLivePdo']) === false):
         return false;
       else:
         $PhpLivePdo = &$Options['PhpLivePdo'];
@@ -23,19 +23,23 @@ class PhpLivePerms{
 
     $return = ['r' => null, 'w' => null, 'o' => null];
     //Get resource id by name
-    if(is_numeric($Options['Resource']) == false):
+    if(is_numeric($Options['Resource']) === false):
       if(session_name() == 'PHPSESSID'):
-        $result = $PhpLivePdo->Run('select resource_id
+        $result = $PhpLivePdo->Run('
+          select resource_id
           from sys_resources
           where resource=?
-            and site is null', [
+            and site is null
+        ',[
           [1, $Options['Resource'], PdoStr]
         ]);
       else:
-        $result = $PhpLivePdo->Run('select resource_id
+        $result = $PhpLivePdo->Run('
+          select resource_id
           from sys_resources
           where resource=?
-            and site=?', [
+            and site=?
+        ',[
           [1, $Options['Resource'], PdoStr],
           [2, session_name(), PdoStr]
         ]);
@@ -43,31 +47,36 @@ class PhpLivePerms{
       $Options['Resource'] = $result[0][0];
     endif;
     // Permissions for everyone
-    $result = $PhpLivePdo->Run('select r,w,o
+    $result = $PhpLivePdo->Run('
+      select r,w,o
       from sys_perms
       where resource_id=?
-        and group_id=1', [
+        and group_id=1
+    ',[
       [1, $Options['Resource'], PdoInt]
     ]);
     if(count($result) > 0):
       $return = $this->SetPerms($return, $result[0]);
     endif;
     // Unauthenticated?
-    if(isset($Options['User']) == false or is_null($Options['User'])):
+    if(isset($Options['User']) === false or $Options['User'] === null):
       return $return;
     endif;
     // Admin?
-    $result = $PhpLivePdo->Run('select *
+    $result = $PhpLivePdo->Run('
+      select *
       from sys_usergroup
       where group_id=3
-        and user_id=?', [
+        and user_id=?
+    ',[
       [1, $Options['User'], PdoInt]
     ]);
-    if(count($result) == 1):
-      return ['r' => 1, 'w' => 1, 'o' => 1];
+    if(count($result) === 1):
+      return ['r' => true, 'w' => true, 'o' => true];
     endif;
     // Others
-    $result = $PhpLivePdo->Run('select r,w,o
+    $result = $PhpLivePdo->Run('
+      select r,w,o
       from sys_perms
       where resource_id=:resource
         and(
@@ -75,7 +84,8 @@ class PhpLivePerms{
           or group_id=2
           or group_id in (select group_id from sys_groups where user_id=:user)
         )
-      order by r,w,o', [
+      order by r,w,o
+    ',[
       [':resource', $Options['Resource'], PdoInt],
       [':user', $Options['User'], PdoInt]
     ]);
@@ -87,13 +97,13 @@ class PhpLivePerms{
   }
 
   private function SetPerms(array $All, array $Perms):array{
-    if($All['r'] !== 0):
+    if($All['r'] !== false):
       $All['r'] = $Perms['r'];
     endif;
-    if($All['w'] !== 0):
+    if($All['w'] !== false):
       $All['w'] = $Perms['w'];
     endif;
-    if($All['o'] !== 0):
+    if($All['o'] !== false):
       $All['o'] = $Perms['o'];
     endif;
     return $All;
