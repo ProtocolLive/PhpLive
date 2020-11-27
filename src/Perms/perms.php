0f7fa7a606e38eefa654aa19a1f0c94efc7c41a0
Protocol Corporation <39626142+ProtocolLive@users.noreply.github.com>
2020-11-27 12:13:42 -0300

    Added support for PHP 8

--- src/Perms/perms.php
+++ src/Perms/perms.php
@@ -1,10 +1,12 @@
 <?php
 // Protocol Corporation Ltda.
 // https://github.com/ProtocolLive/PhpLive/
-// Version 2020.07.23.00
+// Version 2020.11.27.00
 
-if(version_compare(phpversion(), '7.4', '<')):
-  require(__DIR__ . '/perms73.php');
-else:
+if(version_compare(phpversion(), '8', '>=')):
+  require(__DIR__ . '/perms80.php');
+elseif(version_compare(phpversion(), '7.4', '>=')):
   require(__DIR__ . '/perms74.php');
+else:
+  require(__DIR__ . '/perms73.php');
 endif;
