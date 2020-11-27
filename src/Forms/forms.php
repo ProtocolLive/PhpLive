c52f532e284582f3e3d501becafd49c68e799bd6
Protocol Corporation <39626142+ProtocolLive@users.noreply.github.com>
2020-11-27 11:34:54 -0300

    Added support for PHP 8

--- src/Forms/forms.php
+++ src/Forms/forms.php
@@ -1,10 +1,12 @@
 <?php
 // Protocol Corporation Ltda.
 // https://github.com/ProtocolLive/PhpLive/
-// Version 2020.07.23.00
+// Version 2020.11.27.00
 
-if(version_compare(phpversion(), '7.4', '<')):
-  require(__DIR__ . '/forms73.php');
-else:
+if(version_compare(phpversion(), '8', '>=')):
+  require(__DIR__ . '/forms80.php');
+elseif(version_compare(phpversion(), '7.4', '>=')):
   require(__DIR__ . '/forms74.php');
+else:
+  require(__DIR__ . '/forms73.php');
 endif;
