1e1e38b3f8fb24820e38651d98b2f8e35381aec1
Protocol Corporation <39626142+ProtocolLive@users.noreply.github.com>
2020-11-29 21:01:33 -0300

    Removed suport for PHP 7.3

--- src/PDO/pdo.php
+++ src/PDO/pdo.php
@@ -1,12 +1,10 @@
 <?php
 // Protocol Corporation Ltda.
 // https://github.com/ProtocolLive/PhpLive/
-// Version 2020.11.27.00
+// Version 2020.11.29.00
 
 if(version_compare(phpversion(), '8', '>=')):
   require(__DIR__ . '/pdo80.php');
-elseif(version_compare(phpversion(), '7.4', '>=')):
-  require(__DIR__ . '/pdo74.php');
 else:
-  require(__DIR__ . '/pdo73.php');
+  require(__DIR__ . '/pdo74.php');
 endif;
